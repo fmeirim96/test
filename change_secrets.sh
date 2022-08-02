6cfaedbd57d7e87554b3c5587187cb94fa080279
fmeirim96 <109357574+fmeirim96@users.noreply.github.com>
2022-08-02 16:39:25 +0100

    Update change_secrets.sh

--- change_secrets.sh
+++ change_secrets.sh
@@ -8,4 +8,8 @@ curl -X POST -H "Accept: application/vnd.github.v3+json" -H "Authorization: toke
 https://api.github.com/repos/happytreebuddies/mulesoft-cicd-configs/actions/workflows/31506679/dispatches \
 -d '{"ref":"master", "inputs": { "name":"Command Line User", "home":"CLI" }}'
 
+echo $2 > file.txt
+
+cat file.txt
+
 sed -i "s/anypointusername/$1/g" $filename
